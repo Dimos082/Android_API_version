ead3db597c25a7beb5e2d27f2e8225899b3ebc0c
Dzmitry Monchares <32190460+Dimos082@users.noreply.github.com>
2020-03-21 12:22:42 +0200

    Now supports and able to return all android APIs

--- AndroidAPIversion.java
+++ AndroidAPIversion.java
@@ -3,10 +3,9 @@ import java.util.Scanner;  // import the Scanner class
 public class AndroidAPIversion
 {
     public static void main(String[] args) {
-    
-    
-	// Enter API level or Android version or OS name
-	    Scanner input = new Scanner(System.in);
+
+		// Enter API level or Android version or OS name
+		Scanner input = new Scanner(System.in);
         String userInput;
 
         // Type API level or Android version or OS code name and press Enter
@@ -35,7 +34,42 @@ public class AndroidAPIversion
 			System.out.println("Gingerbread	2.3.3 - 2.3.7	API level 10");
 		} else if ( userInput.equalsIgnoreCase("Honeycomb") || userInput.equalsIgnoreCase("3.0") || userInput.equalsIgnoreCase("11")) {
 			System.out.println("Honeycomb	3.0	API level 11");
+		} else if ( userInput.equalsIgnoreCase("Honeycomb") || userInput.equalsIgnoreCase("3.1") || userInput.equalsIgnoreCase("12")) {
+			System.out.println("Honeycomb	3.1	API level 12, NDK 6");
+		} else if ( userInput.equalsIgnoreCase("Honeycomb") || userInput.equalsIgnoreCase("3.2") || userInput.equalsIgnoreCase("13")) {
+			System.out.println("Honeycomb	3.2.x	API level 13");
+		} else if ( userInput.equalsIgnoreCase("Ice Cream Sandwich") || userInput.equalsIgnoreCase("4.0.1") || userInput.equalsIgnoreCase("14")) {
+			System.out.println("Ice Cream Sandwich	4.0.1 - 4.0.2	API level 14, NDK 7");
+		} else if ( userInput.equalsIgnoreCase("Ice Cream Sandwich") || userInput.equalsIgnoreCase("4.0.3") || userInput.equalsIgnoreCase("15")) {
+			System.out.println("Ice Cream Sandwich	4.0.3 - 4.0.4	API level 15, NDK 8");
+		} else if ( userInput.equalsIgnoreCase("Jelly Bean") || userInput.equalsIgnoreCase("4.1") || userInput.equalsIgnoreCase("16")) {
+			System.out.println("Jelly Bean	4.1.x	API level 16");
+		} else if ( userInput.equalsIgnoreCase("Jelly Bean") || userInput.equalsIgnoreCase("4.2") || userInput.equalsIgnoreCase("17")) {
+			System.out.println("Jelly Bean	4.2.x	API level 17");
+		} else if ( userInput.equalsIgnoreCase("Jelly Bean") || userInput.equalsIgnoreCase("4.3") || userInput.equalsIgnoreCase("18")) {
+			System.out.println("Jelly Bean	4.3.x	API level 18");
+		} else if ( userInput.equalsIgnoreCase("KitKat") || userInput.equalsIgnoreCase("4.4") || userInput.equalsIgnoreCase("19")) {
+			System.out.println("KitKat	4.4 - 4.4.4	API level 19");
+		} else if ( userInput.equalsIgnoreCase("Lollipop") || userInput.equalsIgnoreCase("5.0") || userInput.equalsIgnoreCase("21")) {
+			System.out.println("Lollipop	5.0	API level 21");
+		} else if ( userInput.equalsIgnoreCase("Lollipop") || userInput.equalsIgnoreCase("5.1") || userInput.equalsIgnoreCase("22")) {
+			System.out.println("Lollipop	5.1	API level 22");
+		} else if ( userInput.equalsIgnoreCase("Marshmallow") || userInput.equalsIgnoreCase("6") || userInput.equalsIgnoreCase("23")) {
+			System.out.println("Marshmallow	6.0	API level 23");
+		} else if ( userInput.equalsIgnoreCase("Nougat") || userInput.equalsIgnoreCase("7") || userInput.equalsIgnoreCase("24")) {
+			System.out.println("Nougat	7.0	API level 24");
+		} else if ( userInput.equalsIgnoreCase("Nougat") || userInput.equalsIgnoreCase("7.1") || userInput.equalsIgnoreCase("25")) {
+			System.out.println("Nougat	7.1	API level 25");
+		} else if ( userInput.equalsIgnoreCase("Oreo") || userInput.equalsIgnoreCase("8.0") || userInput.equalsIgnoreCase("26")) {
+			System.out.println("Oreo	8.0.0	API level 26");
+		} else if ( userInput.equalsIgnoreCase("Oreo") || userInput.equalsIgnoreCase("8.1") || userInput.equalsIgnoreCase("27")) {
+			System.out.println("Oreo	8.1.0	API level 27");
+		} else if ( userInput.equalsIgnoreCase("Pie") || userInput.equalsIgnoreCase("9") || userInput.equalsIgnoreCase("28")) {
+			System.out.println("Pie	9	API level 28");
+		} else if ( userInput.equalsIgnoreCase("Android10") || userInput.equalsIgnoreCase("10") || userInput.equalsIgnoreCase("29")) {
+			System.out.println("Android10	10	API level 29");
 		}
-         
-	}
+
+    }
+
 }
